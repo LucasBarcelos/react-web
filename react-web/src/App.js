ed32eeb0d5e72467f6b73bc3700ad5b44b862625
FIAP <FIAP>
2019-10-26 15:43:23 -0300

    Criação de router, criação de componente e arquitetura de diretórios

---
+++ react-web/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+import Routes from './routes';
+import Header from './components/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Mercado Livre Teste"/>
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
